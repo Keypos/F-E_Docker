c9a9a7605b70f42745a2c726a2f2b0c52150996c
Jonas Morsch <jonas.morsch@t-online.de>
2022-01-08 11:45:28 +0100

    jonas morsch

---
+++ Docker_Freecad_XFCE/start.sh
@@ -0,0 +1,1 @@
+sudo docker run -it -p 5900:5900 -v "$PWD/shared":/home/user/shared freecad_xfce:0.2
---
+++ Docker_Pycharm_XFCE/start.sh
@@ -0,0 +1,1 @@
+sudo docker run -it -p 5902:5900 -v "$PWD/shared":/home/user/shared pycharm_xfce:0.3
